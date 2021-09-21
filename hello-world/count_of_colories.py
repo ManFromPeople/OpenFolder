812344872f6e063ca01d81514b42566a97d703ea
ManFromPeople <moralsystem@gmail.com>
2021-09-21 14:04:32 +0700

    Add some files for python project

---
+++ hello-world/count_of_colories.py
@@ -0,0 +1,12 @@
+print("What day of week is now? :")
+day_of_week = input()
+print("Breakfast calories :")
+breakfast = int(input())
+print("Lunch calories :")
+lunch = int(input())
+print("Dinner calories :")
+dinner = int(input())
+print("Snack calorites :")
+snacks = int(input())
+sum = breakfast+lunch+dinner+snacks
+print("Calorie content for", day_of_week,":", sum)
--- hello-world/hello.py
+++ hello-world/hello.py
@@ -1,1 +1,4 @@
-print('HEllo, World')
+#print('HEllo, World')
+print("What is your name?")
+name = input()
+print("Hello, " + name)
