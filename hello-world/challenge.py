d90a0e268a45be0a8915c838d705f6f0404bf1c4
ManFromPeople <moralsystem@gmail.com>
2021-09-22 11:46:17 +0700

    Deplore project for python on repos

---
+++ hello-world/challenge.py
@@ -0,0 +1,8 @@
+print("Would you like to continue? Please type yes or no")
+answer = input()
+if answer == 'no' or answer == 'n':
+     print("Программа закрывается")
+elif answer == 'yes' or answer == 'y':
+     print("Можно продолжать дальше выполнение программы")
+else:
+     print("Вы ввели недопустимое значение для продолжения работы")
--- hello-world/test.py
+++ hello-world/test.py
@@ -16,3 +16,69 @@ if not first_number > 1 and second_number < 10:
     print('Both values pass the test')
 else:
     print('Both values do NOT pass the test')
+
+message = 'hello world'
+print(message.lower())
+print(message.upper())
+
+message = message.title()
+print(message)
+print(message.swapcase())
+
+location = 'Mississippi'
+print(location.count('s'))
+
+print(len('how many characters in this string?'))
+
+message = 'racecar'
+print(message.startswith('r'))
+print(message.startswith('a'))
+print(message.startswith('ra'))
+
+print(message.endswith('r'))
+print(message.endswith('a'))
+print(message.endswith('ar'))
+
+message = 'The quick brown fox jumps over the lazy dog'
+print(message.find('q'))
+
+print(message.find('t'))
+print(message.find('T'))
+
+message = '    middle     '
+print('.' + message.lstrip() + '.')
+print('.' + message.rstrip() + '.')
+print('.' + message.strip() + '.')
+
+message = 'brevity is the essence of wit'
+message = message.replace('essence', 'soul')
+print(message)
+
+message = 'howdy'
+print(message.rjust(20))
+print(message.rjust(20, '-'))
+print(message.ljust(20))
+print(message.ljust(20, '-'))
+
+medicine = 'Coughussin'
+dosage = 5
+duration = 4.5
+
+instructions = '{} - Take {} ML by mouth every {} hours'.format(medicine, dosage, duration)
+print(instructions)
+
+instructions = '{2} - Take {1} ML by mouth every {0} hours'.format(medicine, dosage, duration)
+print(instructions)
+
+instructions = '{medicine} - Take {dosage} ML by mouth every {duration} hours'.format(medicine = 'Sneezergen', dosage = 10, duration = 6)
+
+print(instructions)
+
+name = 'World'
+message = f'Hello, {name}.'
+print(message)
+
+count = 10
+value = 3.14
+message = f'Count to {count}.  Multiply by {value}.'
+print(message)
