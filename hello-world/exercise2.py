05172603a2e586804472f52380862cb03668d204
ManFromPeople <moralsystem@gmail.com>
2022-02-28 15:40:34 +0700

    main

--- hello-world/exercise2.py
+++ hello-world/exercise2.py
@@ -1,7 +1,7 @@
 first_number = 5
 second_number = 0
 true_value = True
-false_value = False
+false_value = False 
 
 if first_number > 1 and first_number < 10:
     print('The value is between 1 and 10.')
